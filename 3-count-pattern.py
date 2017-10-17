a5d6aecb7356ee8cd1f4c3ba352fe7c643d92cfa
Julia <juliajune24@gmail.com>
2017-10-17 21:02:44 +0300

    Add third task

--- 3-count-pattern.py
+++ 3-count-pattern.py
@@ -8,12 +8,14 @@ Pattern = str(input())
 LenGenom = len(Genom)
 LenPattern = len(Pattern)
 
+countPattern = 0
 positionPattern = ''
 
 for i in range(LenGenom):
   count = i
   j = 0
   flag = 0
+
   while j < LenPattern and count < LenGenom:
     if Genom[count] == Pattern[j]:
       j += 1
@@ -21,7 +23,9 @@ for i in range(LenGenom):
     else:
       flag = 1
       break
-  if flag == 0 and j == LenPattern:
-    positionPattern += str(i + 1) + ' '
 
-print(positionPattern)
+  if flag == 0 and j == LenPattern:
+    countPattern += 1
+    #positionPattern += str(i + 1) + ' '
+
+print(countPattern)
