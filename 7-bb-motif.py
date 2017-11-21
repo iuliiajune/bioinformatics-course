e7d031ad1128c51df2418217d8bb20fbaf397e93
Julia <juliajune24@gmail.com>
2017-11-21 18:54:32 +0300

    Code review and add output in file

--- 7-bb-motif.py
+++ 7-bb-motif.py
@@ -22,7 +22,6 @@ def NextVertex(a, L, k):
 	global i
 	if i < L:
 		i += 1
-		#a.append(0);
 		return a
 	else:
 		for y in range(L):
@@ -33,7 +32,6 @@ def NextVertex(a, L, k):
 			else:
 				a[j] = 0
 				i -= 1
-				#a.append(a[0:len(a)-1])
 	i = 0
 	return a
 
@@ -41,7 +39,7 @@ def Bypass(a,L,k):
 	global i
 	if i != 0:
 		for y in range(i):
-			j = i - y - 1 #+1
+			j = i - y - 1 
 			if a[j] < k:
 				a[j] += 1
 				return a
@@ -85,7 +83,6 @@ def dH(first, second):
 # функция, считающая Score
 def Score(DNA):
 	global i
-	#print("iScore",i)
 	Length = len(DNA[0])
 
 	A = [0] * (Length)
@@ -93,7 +90,7 @@ def Score(DNA):
 	G = [0] * (Length)
 	T = [0] * (Length)
 
-	for r in range(i): #t
+	for r in range(i):
 		for m in range(Length):
 			z = DNA[r][m]
 			if z == 'A':
@@ -120,7 +117,6 @@ def Concat_piece(piece_txt, set, i):
 	tmp = []
 	for h in range(i):
 		tmp.append(piece_txt[h][set[h]:set[h]+l])
-		#print("tmp",tmp)
 	return tmp
 
 # start a programm
@@ -144,45 +140,41 @@ bestMotifs = []
 
 K = n - l
 i = 1
-f = open('help.txt', 'w')
+#f = open('help.txt', 'w')
 while i > 0:
-#for p in range(1, 80):
-	f.write("i for"+ str(i) + '\n')
-	#s = s[0: i]
-	#print("s", s)
-		
+	# f.write("i for"+ str(i) + '\n')	
 	if i < t:
 		DNA = Concat_piece(text, s, i);
-		f.write("DNA"+str(DNA) + '\n')
+		# f.write("DNA"+str(DNA) + '\n')
 		sco = Score(DNA)
 		optimisticScore = sco + (t - i) * l
-		f.write("optimisticScore"+str(optimisticScore)+ "bestScore"+ str(bestScore) + '\n')
+		# f.write("optimisticScore"+str(optimisticScore)+ "bestScore"+ str(bestScore) + '\n')
 		if optimisticScore < bestScore:
 			s = Bypass(s, t, K)
-			f.write("s by"+ str(s) + '\n')
+			# f.write("s by"+ str(s) + '\n')
 		else:
 			s = NextVertex(s, t, K)
-			f.write("s ne1"+ str(s) + '\n')
+			# f.write("s ne1"+ str(s) + '\n')
 	else:
 		DNA = Concat_piece(text, s, i);
-		f.write("DNA"+ str(DNA) + '\n')
+		# f.write("DNA"+ str(DNA) + '\n')
 		t_score = Score(DNA)
-		f.write("t_score"+str(t_score)+ "bestScore"+ str(bestScore) + '\n')
+		# f.write("t_score"+str(t_score)+ "bestScore"+ str(bestScore) + '\n')
 		if t_score >= bestScore:
 			bestScore = t_score
-			f.write("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!bestScore"+str(bestScore) + '\n')
+			# f.write("\t\t\t\t bestScore"+str(bestScore) + '\n')
 			bestS = s
 			bestMotifs = Concat_piece(text, bestS, t)
 		s = NextVertex(s, t, K)
-		f.write("s ne2"+ str(s) + '\n')
+		# f.write("s ne2"+ str(s) + '\n')
 	
 
-for count in range(t):
-	print(bestMotifs[count])
-
-# # write in file
-# f = open('output.txt', 'w')
 # for count in range(t):
-# 	f.write(str(bestMotifs[count]) + '\n')
+# 	print(bestMotifs[count])
 
-# f.close()
+# write in file
+f = open('output.txt', 'w')
+for count in range(t):
+	f.write(str(bestMotifs[count]) + '\n')
+
+f.close()
