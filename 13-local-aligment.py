8a541ae37b03f3da7977efc78f7f88a293bd3fbc
Julia <juliajune24@gmail.com>
2017-12-11 23:27:38 +0300

    Update local-aligment file

--- 13-local-aligment.py
+++ 13-local-aligment.py
@@ -89,3 +89,111 @@ b = [[0 for i in range(m+1)] for t in range(n+1)]
 #print(b)
 
 lack = 5
+
+for i in range(1,n+1):
+	s[i][0] += s[i-1][0] - lack
+	b[i][0] = 1
+
+for j in range(1, m+1):
+	s[0][j] += s[0][j-1] - lack
+	b[0][j] = 2
+
+for i in range(1,n+1):
+	for j in range(1, m+1):
+		tv = fromAcid(v[i-1])
+		tw = fromAcid(w[j-1])
+
+		a = s[i-1][j] - lack
+		d = s[i][j-1] - lack
+		c = s[i-1][j-1] + scoring[tv][tw]
+
+		if c >= d:
+			if a >= c:
+				if (0 >= a):
+					s[i][j] = 0
+					b[i][j] = 3 # jump
+				else:
+					s[i][j] = a
+					b[i][j] = 1 # up
+			else:
+				if (0 >= c):
+					s[i][j] = 0
+					b[i][j] = 3 # jump
+				else:
+					s[i][j] = c
+					b[i][j] = 0 # 
+				
+		else: # d > c
+			if a >= d:
+				if (0 >= a):
+					s[i][j] = 0
+					b[i][j] = 3 # jump
+				else:
+					s[i][j] = a
+					b[i][j] = 1 # up
+			else: # d > a
+				if (0 >= d):
+					s[i][j] = 0
+					b[i][j] = 3 # jump
+				else:
+					s[i][j] = d
+					b[i][j] = 2 # 
+
+# print("\n")		
+# for i in range(0, n+1):
+# 	print(s[i])
+				
+# print("\n")		
+# for i in range(0, n+1):
+# 	print(b[i])
+
+i_max = 0
+j_max = 0
+s_max = s[0][0]
+
+for i in range(1,n+1):
+	for j in range(1,m+1):
+		if s[i][j] >= s_max:
+			s_max = s[i][j]
+			i_max = i
+			j_max = j
+
+# print("s_max", s_max,"i_max", i_max, "j_max", j_max)
+
+ans1 = ''
+ans2 = ''
+
+i = i_max
+j = j_max
+
+while (b[i][j] != 3 ) & (i > 0) & (j > 0):
+	if b[i][j] == 0:
+		ans1 += w[j-1]
+		ans2 += v[i-1]
+		i -= 1
+		j -= 1
+		
+	else:
+		if b[i][j] == 1:
+			ans1 += '-' 
+			ans2 += v[i-1]
+			i -= 1
+		else:
+			ans1 += w[j-1] 
+			ans2 += '-'
+			j -= 1
+
+res1 = ''
+for y in reversed(ans1):
+	res1 += y
+
+res2 = ''
+for y in reversed(ans2):
+	res2 += y
+
+f = open('output.txt', 'w')
+f.write(str(s_max) + "\n")
+f.write(str(res2)+ "\n")
+f.write(str(res1))
+
+f.close()
