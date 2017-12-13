7bd54fa56704a9ee43b58a047f12882d33586edd
Julia <juliajune24@gmail.com>
2017-12-13 23:26:09 +0300

    Implemented 19-Eulerian-cycle

--- 19-Eulerian-cycle.py
+++ 19-Eulerian-cycle.py
@@ -1,11 +1,10 @@
 # 19-Eulerian-cycle
 
 # input data
-f = open('input12.txt')
+f = open('input.txt')
 v = f.readlines()
 f.close()
 
-# print(v)
 n = len(v)
 
 edge = 0 # count of edges
@@ -14,21 +13,23 @@ d = []
 for i in range(n):
 	w.append(v[i].split(' -> '))
 	d.append([int(w[i][0])])
-	# print(w)
+
 	w[i][1] = w[i][1].split(',')
 	for y in range(len(w[i][1])):
 		d[i].append(int(w[i][1][y]))
 		edge += 1 
 
 # print(d)
-print(edge)
+# print(edge)
 
-matr = [[0 for i in range(n)] for j in range(n)]
+k = d[n-1][0] + 1
+
+matr = [[0 for i in range(k)] for j in range(k)]
 
 for i in range(n):
 	for j in range(1,len(d[i])):
 		matr[d[i][0]][d[i][j]] = 1
-
+	
 # print("\n")		
 # for i in range(n):
 # 	print(matr[i])
@@ -41,55 +42,58 @@ flag = -1
 matr_flag = -1
 while c < edge:
 	matr_flag = -1
-	# flag += 1
-	# ver.append([])
-	# c = 0
-	# while i < n:
-	while j < n:
+
+	while j < k: #n:
 		if matr[i][j] == 1:
 			matr_flag += 1
 			if matr_flag == 0:
 				flag += 1
 				ver.append([])
+
+				y = 0
+				while y < len(ver[flag-1]):
+					# print(ver[flag-1][y])
+					# print("i", i)
+
+					if ver[flag-1][y] == i: #d[i][0]:
+						t_count = 0
+						p = y
+						while p < len(ver[flag-1]):
+							ver[flag].append(ver[flag-1][p])
+							p += 1
+							t_count += 1
+						if t_count < len(ver[flag-1]):
+							u = 0
+							while (u < y):
+								ver[flag].append(ver[flag-1][u])
+								u += 1
+					y += 1
 			matr[i][j] = 2
 			c += 1
-			ver[flag].append(d[i][0])
-			i = d[j][0]
+			ver[flag].append(i) #d[i][0])
+			i = j #d[j][0]
 			j = 0
-		# elif matr[i][j] == 2:
-		# 	matr[i][j] = -1
-		# 	c += 1
-		# 	ver.append(d[i][0])
-		# 	i = d[j][0]
-		# 	j = 0
-		# elif matr[i][j] == 0:
-		# 	j += 1
+
 		else:
 			j += 1
 	i += 1
 	j = 0
-print(ver)
-print(c)
+# print(ver)
 
 m = len(ver)
 
 ans = ''
 i = m-1
 start = str(ver[i][0])
-while i > -1:
-	# for j in range(len(ver[i])):
-	while ver[i][j] != start:
-		j += 1
+
+for j in range(len(ver[i])):
 	ans += str(ver[i][j]) + '->'
-	start = str(ver[i][0])
-	ans += start
-	i -= 1 
+start = str(ver[i][0])
+ans += start
 
-print(ans)
+# print(ans)
 
-# f = open('output.txt', 'w')
-# f.write(str(s_max) + "\n")
-# f.write(str(res2)+ "\n")
-# f.write(str(res1))
+f = open('output.txt', 'w')
+f.write(str(ans))
 
-# f.close()
+f.close()
